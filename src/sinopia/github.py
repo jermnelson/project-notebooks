9e6952fa2ad10df1e5cab4fd1ca4b7ab7ae41517
Jeremy Nelson <jermnelson@gmail.com>
2019-07-26 14:00:41 -0600

    added more content and work on v4 github API

--- src/sinopia/github.py
+++ src/sinopia/github.py
@@ -20,7 +20,19 @@ def all_issues(**kwargs):
     repo -- The name of the LD4P repository (defaults sinopia_editor)
     state -- The state of the issues, (defaults OPEN)
     """
-
+    issues = []
+    repo = repo=kwargs.get('repo', 'sinopia_editor')
+    closed_issues = issues_query(repo=repo,
+                                 state="CLOSED")
+    issues.append(closed_issues)
+    hasNextPage = closed_issues['data']['repository']['issues']['pageInfo']['hasNextPage']
+    while hasNextPage:
+        closed_issues = issues_query(repo=repo,
+            state="CLOSED",
+            after=closed_issues['data']['repository']['issues']['pageInfo']['endCursor'])
+        issues.append(closed_issues)
+        hasNextPage = closed_issues['data']['repository']['issues']['pageInfo']['hasNextPage']
+    return issues
 
 def issues_query(**kwargs):
     """Queries with graph QL for issues and returns json
@@ -29,11 +41,15 @@ def issues_query(**kwargs):
     repo -- The name of the LD4P repository (defaults sinopia_editor)
     state -- The state of the issues, (defaults OPEN)
     first -- number of items from the first (defaults 100)
+    after -- End cursor
     """
+    after = kwargs.get('after', '')
+    if len(after) > 0:
+        after = """, after: "{after}" """.format(after=after)
     query = {
         'query': """{{
             repository(owner:"LD4P", name:"{repo}") {{
-              issues(states:{state}, first:{first}) {{
+              issues(first:{first} {after}) {{
                 pageInfo {{
                     startCursor
                     hasNextPage
@@ -64,6 +80,7 @@ def issues_query(**kwargs):
                             }}
                         }}
                     }}
+                    state
                   }}
                 }}
               }}
@@ -71,7 +88,11 @@ def issues_query(**kwargs):
         }}
     """.format(state=kwargs.get('state', 'OPEN'),
                first=kwargs.get('first', 100),
-               repo=kwargs.get('repo', 'sinopia_editor'))
+               repo=kwargs.get('repo', 'sinopia_editor'),
+               after=after)
     }
     result = requests.post(url=GITHUB_URL, json=query, headers=GITHUB_HEADERS)
-    return result.json()
+    payload = result.json()
+    if "errors" in payload.keys():
+        print(payload)
+    return payload

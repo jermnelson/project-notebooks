1121a854b12bb161c4a45181b3390f9007e4a892
Jeremy Nelson <jermnelson@gmail.com>
2019-07-29 17:31:17 -0600

    Data issues and continue to search commits and pull requests

--- src/sinopia/data.py
+++ src/sinopia/data.py
@@ -11,3 +11,33 @@ def issues_data_frame(**kwargs):
     Keyword arguments:
     issues: list of issues
     """
+    issues = kwargs.get('issues')
+    closed, created, title, state, project, participants = {}, {}, {}, {}, {}, {}
+    for row in issues:
+        node = row['node']
+        created[node['number']] = pd.Timestamp(node['createdAt'])
+        if node.get('closedAt') is not None:
+            closed[node['number']] = pd.Timestamp(node['closedAt'])
+        title[node['number']] = node['title']
+        state[node['number']] = node['state']
+        # Add project
+        if len(node["projectCards"]["edges"]) > 0:
+            projects = []
+            for project_row in node["projectCards"]["edges"]:
+                projects.append(project_row['node']['project']['number'])
+            project[node['number']] =  projects
+        # Add participants
+        agents = []
+        for participant in node['participants']['edges']:
+            agents.append(participant['node']['login'])
+        participants[node['number']] = agents
+    df = pd.DataFrame({
+        'created': created,
+        'closed': closed,
+        'participants': participants,
+        'project': project,
+        'title': title,
+        'state': state
+    })
+    df['elapsed'] = df['closed'] - df['created']
+    return df
--- src/sinopia/github.py
+++ src/sinopia/github.py
@@ -34,6 +34,66 @@ def all_issues(**kwargs):
         hasNextPage = closed_issues['data']['repository']['issues']['pageInfo']['hasNextPage']
     return issues
 
+def __get_after__(**kwargs):
+    """Helper function returns string for Graph QL after"""
+    after = kwargs.get('after', '')
+    if len(after) > 0:
+        after = """, after: "{after}" """.format(after=after)
+    return after
+
+def commits_query(**kwargs):
+    """Queries repository for commits and returns JSON
+
+    Keyword arguments:
+    repo -- The name of the LD4P repository (defaults sinopia_editor)
+    branch -- Branch (defaults to master)
+    state -- The state of the issues, (defaults OPEN)
+    first -- number of items from the first (defaults 100)
+    after -- End cursor
+    """
+    after = __get_after__(**kwargs)
+    query = {
+        'query': """{{
+            repository(owner:"LD4P", name:"{repo}") {{
+              ref(qualifiedName: {branch}) {{
+                target {{
+                  ... on Commit {{
+                     id
+                     history(first: {first}, {after}) {{
+                       pageInfo {{
+                        startCursor
+                        hasNextPage
+                        endCursor
+                       }}
+                      edges {{
+                        node {{
+                          messageHeadline
+                          oid
+                          message
+                          author {{
+                            user {{
+                              login
+                            }}
+                            date
+                          }}
+                        }}
+                      }}
+                     }}
+                  }}
+                }}
+              }}
+            }}
+        }}""".format(repo=kwargs.get('repo', 'sinopia_editor'),
+                     branch=kwargs.get('branch', 'master'),
+                     first=kwargs.get('first', 100),
+                     after=after)
+    }
+    result = requests.post(url=GITHUB_URL, json=query, headers=GITHUB_HEADERS)
+    payload = result.json()
+    if "errors" in payload.keys():
+        print(payload)
+    return payload
+
 def issues_query(**kwargs):
     """Queries with graph QL for issues and returns json
 
@@ -43,9 +103,7 @@ def issues_query(**kwargs):
     first -- number of items from the first (defaults 100)
     after -- End cursor
     """
-    after = kwargs.get('after', '')
-    if len(after) > 0:
-        after = """, after: "{after}" """.format(after=after)
+    after = __get_after__(**kwargs)
     query = {
         'query': """{{
             repository(owner:"LD4P", name:"{repo}") {{
@@ -96,3 +154,65 @@ def issues_query(**kwargs):
     if "errors" in payload.keys():
         print(payload)
     return payload
+
+def pull_requests_query(**kwargs):
+    """Queries repository for pull requests and returns JSON
+
+    Keyword arguments:
+    repo -- The name of the LD4P repository (defaults sinopia_editor)
+    first -- number of items from the first (defaults 100)
+    after -- End cursor
+    """
+    after = __get_after__(**kwargs)
+    query = {
+        'query': """{{
+            repository(owner:"LD4P", name:"{repo}") {{
+                pullRequests(baseRefName: "master", first:, {first} {after}) {{
+                    pageInfo {{
+                        startCursor
+                        hasNextPage
+                        endCursor
+                    }}
+                    edges {{
+                      node {{
+                        closedAt
+                        commits(first: 100) {{
+                           edges {{
+                              node {{
+                                  commit {{
+                                    oid
+                                    committedDate
+                                  }}
+                              }}
+                          }}
+                        }}
+                        createdAt
+                        number
+                        state
+                      }}
+                    }}
+                }}
+            }}
+        }}""".format(repo=kwargs.get('repo', 'sinopia_editor'),
+                     first=kwargs.get('first', 100),
+                     after=after)
+    }
+    result = requests.post(url=GITHUB_URL, json=query, headers=GITHUB_HEADERS)
+    payload = result.json()
+    if "errors" in payload.keys():
+        print(payload)
+    return payload    
+
+def write_closed_issues(all_closed):
+    """Takes a listing of all closed issues and persists JSON to filesystem
+
+    Arguments:
+    all_closed -- list of issues from all_issues functions
+    """
+    for i, row in enumerate(all_closed):
+        i+=1
+        filename = f"../data/json/issues-closed-{i:03}.json"
+        if os.path.exists(filename):
+            continue
+        with open(filename, "w+") as fo:
+            json.dump(row['data']['repository']['issues']['edges'], fo, indent=2, sort_keys=True)
